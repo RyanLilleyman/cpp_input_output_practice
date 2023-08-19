5f67e951f384b539c8f32d969b4587e0d3fcd87c
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-08-19 18:58:46 -0400

    Add input output practice

---
+++ sum_of_three.cpp
@@ -0,0 +1,17 @@
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+  int first;
+  int second;
+  int third;
+  printf("Enter the three numbers to sum together!");
+  cin >> first >> second >> third;
+  cout << first + second + third;
+  return 0;
+}
